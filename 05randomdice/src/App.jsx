41508f0677e283690b420ec3e97526b3ef87c136
Ammar Mirza <itsammarmirza@gmail.com>
2023-12-08 20:43:06 +0530

    Optimized the Image changing mechanism a bit

--- 05randomdice/src/App.jsx
+++ 05randomdice/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(1)
-  const images = ['./src/images/one.png', './src/images/two.png', './src/images/three.png', './src/images/four.png', './src/images/five.png', './src/images/six.png']
+  const images = ['one.png', 'two.png', 'three.png', 'four.png', 'five.png', 'six.png']
   
   function generateRandomNumber () {
     setCount(Math.floor(Math.random() * 6) + 1)
@@ -11,7 +11,7 @@ function App() {
 
   return (
       <div className='flex mt-28 gap-12 justify-center items-center'>
-        <img className='h-40' src={images[count - 1]}/>
+        <img className='h-40' src={`./src/images/${images[count - 1]}`}/>
         <button onClick={generateRandomNumber} className='bg-purple-700 p-4 rounded-xl text-white font-semibold'>Roll The Dice</button>
       </div>
   )
