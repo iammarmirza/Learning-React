b46ede9e1c32bc1d7b9bd5d1a60770ad7faa4776
Ammar Mirza <itsammarmirza@gmail.com>
2023-12-12 19:59:01 +0530

    Some Changes

--- 07todoapp/src/App.jsx
+++ 07todoapp/src/App.jsx
@@ -1,29 +1,37 @@
-import { useState, useRef, useEffect, createElement } from 'react'
-import './App.css'
-import List from './components/list'
+import { useState, useRef } from 'react';
+import './App.css';
+import List from './components/list';
 
 function App() {
-  const [task, setTask] = useState('')
-  const inputBox = useRef(null)
+  const [tasks, setTasks] = useState([]);
+  const inputBox = useRef(null);
   
-  const addTask = () => {
-    if(inputBox.current.value !== ''){
-      setTask(inputBox.current.value)
+  const addTask = (e) => {
+    const text = inputBox.current.value;
+    if (text !== '') {
+      setTasks([...tasks, text]);
+      inputBox.current.value = ''; 
     }
-  }
+  };
 
   return (
     <>
-      <div className='flex flex-col h-screen w-screen items-center p-6 bg-[#22092C] gap-12'>
-        <h1 className='font-semibold text-white text-8xl'>To Do List</h1>
-        <div className='flex gap-3'>
-          <input ref={inputBox} className='w-[600px] h-16 rounded-lg px-5 text-2xl font-semibold' placeholder='Enter your task here'/>
-          <button onClick={addTask} className='text-white bg-green-700 text-2xl font-semibold px-5 rounded-lg'>Add Task</button>
+      <div className='flex flex-col min-h-screen w-screen p-6 bg-[#22092C] gap-12'>
+        <h1 className='font-semibold text-white text-8xl text-center'>To Do List</h1>
+        <div className='flex gap-3 self-center'>
+          <input
+            ref={inputBox}
+            className='w-[600px] h-16 rounded-lg px-5 text-2xl font-semibold'
+            placeholder='Enter your task here'
+          />
+          <button onClick={addTask} className='text-white bg-green-700 text-2xl font-semibold px-5 rounded-lg'>
+            Add Task
+          </button>
         </div>
-        <List text={task}/>
+        <List tasks={tasks} />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- 07todoapp/src/components/list.jsx
+++ 07todoapp/src/components/list.jsx
@@ -1,13 +1,19 @@
-function List ({text}) {
-    return (
-        <>
-            <div className="flex gap-10 text-white items-center font-semibold text-2xl">
-                <p className="">{text}</p>
-                <button className="bg-red-500 px-4 py-2 rounded-lg">Delete</button>
+import React from 'react';
+
+function List({ tasks }) {
+
+  return (
+    <>
+      <div className='flex flex-col gap-10 text-white items-center font-semibold text-2xl'>
+        {tasks.map(task => (
+            <div className='flex items-center justify-between w-[800px] text-3xl'>
+                <p>{task}</p>
+                <button className='text-white bg-red-500 px-3 py-2 rounded-lg'>Delete</button>
             </div>
-            
-        </>
-    )
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default List
+export default List;
