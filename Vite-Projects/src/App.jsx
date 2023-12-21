3fdfa776e68235ece11a7c21b36cf5545e0cb9e1
Ammar Mirza <itsammarmirza@gmail.com>
2023-12-21 23:06:37 +0530

    Moved existing code to a new repo

--- Vite-Projects/src/App.jsx
+++ Vite-Projects/src/App.jsx
@@ -4,12 +4,11 @@ import BgChanger from './Projects/BgChanger/BgChanger'
 import RandomDice from './Projects/RandomDice/RandomDice';
 import PasswordGenerator from './Projects/PasswordGenerator/PasswordGenerator';
 import Todo from './Projects/Todo/Todo';
-import SnakesAndLadders from './Projects/SnakesandLadders/SnakesAndLadders';
+
 
 function App() {
   return (
     <>
-      <SnakesAndLadders />
       {/* <Counter /> */}
       {/* <BgChanger /> */}
       {/* <RandomDice /> */}
